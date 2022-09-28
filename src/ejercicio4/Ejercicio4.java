984a13fc1a8e39009fe875368cd3f3d59e12040c
15Carmen <carmen.martin@iesnervion.es>
2022-09-28 14:23:33 +0200

    commit3

--- src/ejercicio4/Ejercicio4.java
+++ src/ejercicio4/Ejercicio4.java
@@ -10,6 +10,17 @@ public class Ejercicio4 {
         //Declaro las variables
         boolean lluvia, tarea, biblio;
 
+        //Le preguntamos al ususario si llueve, si ha hecho la tarea y si tiene que ir a la biblioteca
+        System.out.println("¿Está lloviendo? Conteste con true o false");
+        lluvia= sc.nextBoolean();
+        System.out.println("¿Has hecho la tarea? Conteste con true o false");
+        tarea= sc.nextBoolean();
+        System.out.println("¿Tienes que ir a la biblioteca? Conteste con true o false");
+        biblio= sc.nextBoolean();
+
+        System.out.println(!lluvia&&tarea || biblio);
+
+
 
     }
 }
